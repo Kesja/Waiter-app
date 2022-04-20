7be75e6ae91126d4d95bfcc97c8cb0519e94a589
Joanna Bach <joannakatarzynabach@gmail.com>
2022-04-20 21:24:58 +0200

    Change TableForm

--- src/components/features/TableForm/TableForm.js
+++ src/components/features/TableForm/TableForm.js
@@ -1,20 +1,21 @@
 import { Form, Col, Button, Row} from "react-bootstrap";
-import { getTableStatus} from "../../../redux/tablesRedux";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useSelector } from 'react-redux';
+import { getTableStatus } from "../../../redux/tablesRedux";
 
 
 const TableForm = props => {
-  const {status} = useParams();
-  const tableStatus = useSelector(state => getTableStatus(state, status));
+  const [status, setStatus] = useState(props.status);
+ const allStatuses = useSelector(getTableStatus)
 
   return (
     <Form key={props.id}>
       <Form.Group as={Row} style={{ maxWidth: '18rem' }}>
         <Form.Label column sm="3">Status:</Form.Label>
         <Col sm="9" className='d-flex mb-2'>
-            <Form.Select>
-            <option>{status}</option>
+            <Form.Select value={status} onChange={e => setStatus(e.target.value)}>
+            <option>{props.status}</option>
+            {allStatuses.map((status) => (status !== props.status ? <option key={status}>{status}</option> : ''))}
             </Form.Select>
           </Col>
         <Form.Label column sm="3">People:</Form.Label>
